6a2bae3b1932ba4a25c9e7c04325670f3f21b6f9
miguel myers <myers.m007@gmail.com>
2017-05-29 13:37:15 -0400

    landing page image

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ApplicationRecord
   validates :audio, presence: true
 
-  has_attached_file :audio
+  has_attached_file :audio, :use_timestamp => false
   validates_attachment_content_type :audio, :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
 
   #validates_attachment_content_type :avatar, :content_type => /.*/
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :owners
   resources :songs
+  root to: 'visitors#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
