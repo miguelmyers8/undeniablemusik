53b16694d36e8f4726b2f0200cc235dd833bedc9
miguel myers <myers.m007@gmail.com>
2017-05-30 16:23:57 -0400

    compressing img

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :owners
   resources :songs
   root to: 'visitors#index'
+  get '/visitors/founder'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
