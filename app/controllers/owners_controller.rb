27340d7bb617dc177d05ee2fe8199c8d3241525a
miguel myers <myers.m007@gmail.com>
2017-05-31 19:11:45 -0400

    founders page

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -69,6 +69,6 @@ class OwnersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def owner_params
-      params.require(:owner).permit(:name, :content)
+      params.require(:owner).permit(:image, :name, :content)
     end
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,2 +1,5 @@
 class Owner < ApplicationRecord
+  has_attached_file  :image,
+  styles: {large: "640x640>", medium: "300x300>", thumb: "150x150#" }, :use_timestamp => false
+validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
