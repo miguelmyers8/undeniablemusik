0cbce5c42e302740ba36a7a0824d128af7479e92
miguel myers <myers.m007@gmail.com>
2017-06-03 13:33:00 -0400

    recommit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :owners
   resources :songs
   root to: 'visitors#index'
-  get '/visitors/founder'
+  get '/visitors/rob'
+  get '/visitors/youn'
+  get '/visitors/amhad'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
